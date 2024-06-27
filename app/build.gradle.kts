04a8e1696a16f394acc0490fddab77e2d14ce2a7
MohamedMohamed <m01126165016@icloud.com>
2024-06-27 18:19:56 +0300

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("com.google.code.gson:gson:2.11.0")
+
 }
